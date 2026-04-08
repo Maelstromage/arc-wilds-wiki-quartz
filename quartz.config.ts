bc0dd6dd8b84875660817cfc2b70ac7bf2e81746
Harley Schaeffer <harleyschaeffer@gmail.com>
2026-04-08 15:34:22 -0400

    Update site title and base URL

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "null",
     },
     locale: "en-US",
-    baseUrl: "wiki.gameonmarathon.com",
+    baseUrl: "dndwiki.gameonmarathon.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
